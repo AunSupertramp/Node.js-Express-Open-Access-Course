320ada7c3436d7c59e61a1aa3144bd3a682b243a
AunSupertramp <aunsupertramp@gmail.com>
2023-01-04 23:11:44 +0700

    Update Index.html

--- app.js
+++ app.js
@@ -2,19 +2,23 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
+
+
 const app = express();
-const port =3000;
+const port = 3000;
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, "/public/")));
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
 
     res.send("Hello World");
 
 })
 
-app.listen(port,()=>{
-debug("Listening on port "+ chalk.green(port) );
+app.listen(port, () => {
+    debug("Listening on port " + chalk.green(port));
 
 
 
